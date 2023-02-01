6f72e9a2d4e151869d336afc04158f97601d8e33
vietnh <vietnguyenhoang@vccorp.vn>
2023-02-01 10:53:20 +0700

    update dotnet sample

---
+++ dotnet/sample-dotnet/README.md
@@ -0,0 +1,11 @@
+# Dotnet Core Sample App using ASPNet
+
+Đây là repo code mẫu cho một ứng dụng DotNet triển khai trên nền tảng BizflyCloud App Engine
+
+Tham khảo thêm cách thức triển khai ứng dụng DotNet tại đây
+
+https://docs.bizflycloud.vn/app_engine/deploy/dotnet/
+
+Nhiều hơn các code mẫu về ngôn ngữ DotNet tại đây
+
+https://github.com/bizflycloud/app-engine-samples-code/tree/master/dotnet
--- dotnet/sample-dotnet/Startup.cs
+++ dotnet/sample-dotnet/Startup.cs
@@ -1,40 +1,46 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace helloworld
+namespace aspnet
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("hello, world");
+                    await context.Response.WriteAsync(@"<!DOCTYPE html>
+<html>
+  <head>
+    <title>App Engine BizflyCloud</title>
+  </head>
+  <body>
+    <img style=""display: block; margin-left: auto; margin-right: auto; width: 50%;"" src=""https://techvccloud.mediacdn.vn/zoom/600_315/2018/12/5/logofacebook-1543996079655547158445-crop-1543996085792308014951.png""></img>
+  </body>
+</html>");
                 });
             });
         }
